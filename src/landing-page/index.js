ae87d82de26514af953d7c7bbd82f5e9655371c6
Paulo Ragonha <paulo@ragonha.me>
2016-05-16 23:47:11 +0200

    Menu and focus text

--- src/landing-page/index.js
+++ src/landing-page/index.js
@@ -5,6 +5,17 @@ export default React.createClass({
   render () {
     return (
       <main>
+        <nav>
+          <ul>
+            <li>
+              <a href='https://github.com/saguijs/sagui'>Github</a>
+            </li>
+            <li>
+              <a href='https://www.npmjs.com/package/sagui'>NPM</a>
+            </li>
+          </ul>
+        </nav>
+
         <header>
           <img src={sagui} />
           <h1>Sagui </h1>
@@ -13,21 +24,30 @@ export default React.createClass({
         </header>
 
         <article>
-          <h1>Creating a new front-end project</h1>
-          <p>On a new folder, <strong>create a new project</strong>.</p>
-          <code>
-            npm init -y
-          </code>
+          <section>
+            <h1>Creating a new front-end project</h1>
+            <p>On a new folder, <strong>create a new project</strong>.</p>
+            <code>
+              npm init -y
+            </code>
 
-          <p>Install Sagui as a <strong>development dependency</strong>.</p>
-          <code>
-            npm install --save-dev sagui
-          </code>
+            <p>Install Sagui as a <strong>development dependency</strong>.</p>
+            <code>
+              npm install --save-dev sagui
+            </code>
 
-          <p>And <strong>start</strong> the development server</p>
-          <code>
-            npm start
-          </code>
+            <p>And <strong>start</strong> the development server</p>
+            <code>
+              npm start
+            </code>
+          </section>
+        </article>
+
+        <article>
+          <section>
+            <h1>Focus on your code</h1>
+            <p>Sagui is all about good defaults. It is the single development dependency you need to worry about, taking care of build, tests and the development server.</p>
+          </section>
         </article>
       </main>
     )
